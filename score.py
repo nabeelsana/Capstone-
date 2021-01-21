ede66f09bdb8a5b0da3fe5aa2ee30133294bbbb9
nabeelsana <76969852+nabeelsana@users.noreply.github.com>
2021-01-21 19:34:14 +0000

    Add files via upload

---
+++ score.py
@@ -0,0 +1,31 @@
+import joblib
+import numpy as np
+import os
+
+from inference_schema.schema_decorators import input_schema, output_schema
+from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
+
+
+# The init() method is called once, when the web service starts up.
+#
+# Typically you would deserialize the model file, as shown here using joblib,
+# and store it in a global variable so your run() method can access it later.
+def init():
+    global model
+
+    # The AZUREML_MODEL_DIR environment variable indicates
+    # a directory containing the model file you registered.
+    model_filename = 'model.pkl'
+    model_path = os.path.join(os.environ['AZUREML_MODEL_DIR'], model_filename)
+
+    model = joblib.load(model_path)
+
+
+# The run() method is called each time a request is made to the scoring API.
+data= './data.json'
+def run(data):
+    # Use the model object loaded by init().
+    result = model.predict(data)
+
+    # You can return any JSON-serializable object.
+    return result.tolist()
